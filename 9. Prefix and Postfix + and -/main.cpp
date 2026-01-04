575f322760bb5a5e17952660923f0b36b2e15e5f
jrogb <grobbelaarjohan65@gmail.com>
2026-01-04 14:35:58 +0200

    Add demonstration of prefix and postfix increment/decrement operators in C++
    - Implemented a C++ program to illustrate the use of prefix (++var, --var) and postfix (var++, var--) increment and decrement operators.
    - Included examples of standard increment and decrement for comparison.
    - Provided clear output statements to show the difference in behavior between prefix and postfix operations.

---
+++ 9. Prefix and Postfix + and -/main.cpp
@@ -0,0 +1,46 @@
+/*
+Prefix and Postfix ++ and -- Operators in C++
+This program demonstrates the use of prefix and postfix increment and decrement operators in C++.
+
+Prefix operators (++var, --var) increment or decrement the value of the variable before it is used in an expression.
+Postfix operators (var++, var--) increment or decrement the value of the variable after it is used in an expression.
+
+*/
+
+#include <iostream>
+#include <cmath>
+
+int main() { 
+    //Variable declarations
+    int value {5};
+
+    //Regular increment and decrement
+    //Increment by one
+    int value1 = value + 1; //Standard increment
+    std::cout << "Standard increment: The value is " << value << " incrementing by 1 using value = value + 1; the result is " << value1 << std::endl;
+    value = 5; //Reset value
+    std::cout << "\n";
+    //Decrement by one
+    int value2 = value - 1; //Standard decrement
+    std::cout << "Standard decrement: The value is " << value << " decrementing by 1 using value = value - 1; the result is " << value2 << std::endl;
+    value = 5; //Reset value
+    std::cout << "\n";
+
+    //Postfix increment
+    std::cout << "Postfix increment: The value is " << value++ << " incrementing by 1 using value++; the result is " << value << std::endl; //Note the value used before increment
+    value = 5; //Reset value
+    std::cout << "\n";
+    //Postfix decrement
+    std::cout << "Postfix decrement: The value is " << value-- << " decrementing by 1 using value--; the result is " << value << std::endl; //Note the value used before decrement
+    value = 5; //Reset value
+    std::cout << "\n";
+    //Prefix increment
+    std::cout << "Prefix increment: The value is " << value << " incrementing by 1 using ++value; the result is " << ++value << std::endl; //Note the value used after increment
+    value = 5; //Reset value
+    std::cout << "\n";
+    //Prefix decrement
+    std::cout << "Prefix decrement: The value is " << value << " decrementing by 1 using --value; the result is " << --value << std::endl; //Note the value used after decrement
+    value = 5; //Reset value
+
+    return 0;
+}
